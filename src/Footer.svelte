8f3b7711d89d23613083e4f021f72fed147b8efc
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-01-14 10:22:45 +0530

    fix: name change

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -4,8 +4,8 @@
     <p class="tamil">இங்கு பயன்படுத்தப்படும் திருக்குறளின் விவரங்கள் இந்த <a href="https://github.com/tk120404/thirukkural">Repo</a>-வில் இருந்து பெறப்பட்டது...</p>
     <p class="english">The data used here is borrowed from this <a href="https://github.com/tk120404/thirukkural">Repo</a>...</p><br/>
 
-    <p class="tamil bold">தமிழகராதி © {new Date().getFullYear()}</p>
-    <p class="english bold">Tamizhagarathi © {new Date().getFullYear()}</p><br/>
+    <p class="tamil bold">தமிழிலக்கியம் © {new Date().getFullYear()}</p>
+    <p class="english bold">Tamizhilakkiyam © {new Date().getFullYear()}</p><br/>
 
-    <a class="english" href="https://github.com/tamizhagarathi/tirukural">Github</a>
+    <a class="english" href="https://github.com/tamizhilakkiyam/tirukural">Github</a>
 </div>
